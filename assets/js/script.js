6808198a5b2d7ea755bb9aae013b9970ba66c900
NhatLinh2405 <nhatlinh240501@gmail.com>
2021-11-16 21:10:06 +0700

    done swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,24 @@ document.querySelector('#bar-btn').onclick = () =>{
     barForm.classList.toggle('active');
 }
 
+
+var swiper = new Swiper(".container1", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
